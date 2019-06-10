76dff64a92fa7d6bb5c84cfc8fdc827d6b813e81
cristinam <mateescu.cristina24@yahoo.ro>
2019-06-10 15:23:38 +0300

    initial commit

---
+++ src/CheckMaxNumber.java
@@ -0,0 +1,16 @@
+public class CheckMaxNumber {
+ public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if (a<b) {
+            System.out.println("b este mai mare");
+        } else if (b<a) {
+            System.out.println("a mai mare");
+        } else {
+            System.out.println("egale");
+        }
+        }
+    }
+
+
+
